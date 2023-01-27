03896ae31af1e7dc5eda3b014714eb48a71c5e15
Dai MIKURUBE <dmikurube@acm.org>
2023-01-27 17:26:01 +0900

    Use java.toolchain.languageVersion to set the Java version

--- calql-query/build.gradle.kts
+++ calql-query/build.gradle.kts
@@ -20,8 +20,9 @@ tasks.withType<JavaCompile> {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 
     withJavadocJar()
     withSourcesJar()
