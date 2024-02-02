0bbcec481947c15a407075a2132c61687100921f
Dai MIKURUBE <dmikurube@acm.org>
2024-02-02 22:51:04 +0900

    Upgrade JUnit to 5.10.1

--- calql-query/build.gradle.kts
+++ calql-query/build.gradle.kts
@@ -29,9 +29,9 @@ java {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
 
 tasks.javadoc {
